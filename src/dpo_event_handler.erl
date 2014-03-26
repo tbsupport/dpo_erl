25664cc17459ab724d5eeec3306f68503e0d398e
palkan <dementiev@webils.ru>
2014-03-26 19:11:03 +0400

    fix again

--- src/dpo_event_handler.erl
+++ src/dpo_event_handler.erl
@@ -30,7 +30,8 @@ start_link() ->
 %% @private
 
 listen() ->
-  %%(catch ?I({ems_event_handler,ems_event:add_sup_handler(?MODULE, [])})),
+  ?I({ems_event, code:is_loaded(ems_event)}),
+  (catch ?I({ems_event_handler,ems_event:add_sup_handler(?MODULE, [])})),
   receive
     Msg -> ?E({this_should_not_happen, Msg})
   end.
